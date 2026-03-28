dd650887548cbc25e9f5ccd8b1c024d5e4e44c68
Andrii Shukalo <shukaload@gmail.com>
2026-03-28 23:29:52 +0200

    Fixes 1.0.2

--- task.sql
+++ task.sql
@@ -2,12 +2,11 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders(CustomerID, Date)
+    VALUES (1, "2026-01-01");
 -- Start the transaction 
 START TRANSACTION;
 -- And some data should be created inside the transaction
-
-INSERT INTO Orders(CustomerID, Date)
-    VALUES (1, "2026-01-01");
 
 INSERT INTO OrderItems(OrderID, ProductID, Count)
     VALUES (LAST_INSERT_ID(), 1, 1);
